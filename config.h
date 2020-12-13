1a66119298ee28955dfb234d6b67eea02199bd5d
Akarumei <akarumei@protonmail.com>
2020-12-13 13:17:38 +0500

    patch fix

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#3f0077";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
-	"st", NULL,
+	"bgsh", NULL,
 	NULL /* terminate */
 };
 
