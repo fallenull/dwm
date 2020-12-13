86ad92a67d131116db0dc1b371b8de7f0e0b3bf3
Akarumei <akarumei@protonmail.com>
2020-12-13 13:13:33 +0500

    Added patch "autostart"
    Patch by bit69tream (https://gitlab.com/bit6tream)

--- config.h
+++ config.h
@@ -5,17 +5,22 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "terminus:size=13" };
-static const char dmenufont[]       = "terminus:size=13";
+static const char *fonts[]          = { "terminus:size=12" };
+static const char dmenufont[]       = "terminus:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#3f0077";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+static const char *const autostart[] = {
+	"st", NULL,
+	NULL /* terminate */
 };
 
 /* tagging */
