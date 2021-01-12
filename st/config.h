e4ba7e64e3aa3d19ddef375ca1288ef09756f67c
akmtxc <akmtxc@disroot.org>
2021-01-13 02:18:27 +0500

    Ah shit here we go again

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "terminus:pixelsize=15:antialias=true:autohint=true";
+static char *font = "ibm plex mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
